b686b9aba4f84cbcd13e4da2fcdc42932da2adfb
Cupramora <dewntn@gmail.com>
2025-06-27 12:34:41 -0700

    Revert "Add files via upload"
    This reverts commit 473697f5cd15c5371375f21496923d2cf20a4a2c.

--- ear.py
+++
@@ -1,7 +0,0 @@
-from sound import listen
-
-def process_audio(threshold=0.2):
-    data = listen()
-    if data["volume"] > threshold:
-        return {"event": "loud_noise", "intensity": data["volume"]}
-    return {"event": "ambient", "intensity": data["volume"]}
